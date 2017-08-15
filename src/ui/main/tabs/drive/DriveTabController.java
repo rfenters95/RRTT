8de00402ca6945f908c2d07387a282491b7aec8d
reedfenters <rfenters95@gmail.com>
2017-08-14 21:14:10 -0400

    updated driveTabController logic

--- src/ui/main/tabs/drive/DriveTabController.java
+++ src/ui/main/tabs/drive/DriveTabController.java
@@ -48,6 +48,51 @@ public class DriveTabController implements Initializable, Injectable {
     @FXML
     private JFXButton reverseButton;
 
+    private boolean isValid() {
+
+        RoombaDriveMode driveMode = driveModeComboBox.getSelectionModel().getSelectedItem();
+        boolean hasEmptyFields = input1TextField.getText().isEmpty() || input2TextField.getText().isEmpty();
+        if (hasEmptyFields) return false;
+
+        input1TextField.setText(input1TextField.getText().replaceFirst("^0+(?!$)", ""));
+        input2TextField.setText(input2TextField.getText().replaceFirst("^0+(?!$)", ""));
+
+        if (driveMode == RoombaDriveMode.DRIVE) {
+            int velocity = Integer.parseInt(input1TextField.getText());
+            int radius = Integer.parseInt(input2TextField.getText());
+            boolean isValid;
+            isValid = velocity >= 0 && velocity <= 500;
+            isValid = isValid && radius >= 0 && radius <= 2000;
+            return isValid;
+        }
+        if (driveMode == RoombaDriveMode.DRIVE_DIRECT) {
+            int velocityLeft = Integer.parseInt(input1TextField.getText());
+            int velocityRight = Integer.parseInt(input2TextField.getText());
+            boolean isValid;
+            isValid = velocityLeft >= 0 && velocityLeft <= 500;
+            isValid = isValid && velocityRight >= 0 && velocityRight <= 500;
+            return isValid;
+        }
+
+        return false;
+
+    }
+
+    private void resetText(RoombaDriveMode driveMode) {
+
+        if (driveMode == RoombaDriveMode.DRIVE) {
+            input1TextField.setPromptText("Velocity (mm/s)");
+            input2TextField.setPromptText("Radius (mm)");
+        } else if (driveMode == RoombaDriveMode.DRIVE_DIRECT) {
+            input1TextField.setPromptText("Velocity (mm/s) - Left");
+            input2TextField.setPromptText("Velocity (mm/s) - Right");
+        }
+
+        input1TextField.setText("");
+        input2TextField.setText("");
+
+    }
+
     @Override
     public void inject(RootController rootController) {
         this.rootController = rootController;
@@ -56,31 +101,29 @@ public class DriveTabController implements Initializable, Injectable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        /*
+        /* ********************************************
         *
         * ComboBox logic
         *
-        * */
+        ********************************************* */
 
         driveModeComboBox.getItems().setAll(RoombaDriveMode.values());
         driveModeComboBox.setOnAction(e -> {
             RoombaDriveMode driveMode = driveModeComboBox.getSelectionModel().getSelectedItem();
             if (driveMode == RoombaDriveMode.DRIVE) {
                 rootController.console.appendText("Starting drive...\n");
-                input1TextField.setPromptText("Velocity (mm/s)");
-                input2TextField.setPromptText("Radius (mm)");
+                resetText(driveMode);
             } else if (driveMode == RoombaDriveMode.DRIVE_DIRECT) {
                 rootController.console.appendText("Starting drive direct...\n");
-                input1TextField.setPromptText("Velocity (mm/s) - Left");
-                input2TextField.setPromptText("Velocity (mm/s) - Right");
+                resetText(driveMode);
             }
         });
 
-        /*
+        /* ********************************************
         *
         * TextField logic
         *
-        * */
+        ********************************************* */
 
         input1TextField.textProperty().addListener((observable, oldValue, newValue) -> {
             if (!newValue.matches("\\d{0,4}?")) {
@@ -94,17 +137,20 @@ public class DriveTabController implements Initializable, Injectable {
             }
         });
 
-        /*
+        /* ********************************************
         *
         * Button logic
         *
-        * */
+        ********************************************* */
 
         //TODO perform a input value check; 0 - 500 (mm/s); 0 - 2000 (mm);
 
         forwardButton.setOnAction(e -> {
-            rootController.console.appendText("Test!\n");
-            // RoombaJSSCSingleton.getRoombaJSSC().stop();
+            if (isValid()) {
+                rootController.console.appendText("Test!\n");
+            } else {
+                rootController.console.appendText("Invalid input!\n");
+            }
         });
 
         reverseButton.setOnAction(e -> {
