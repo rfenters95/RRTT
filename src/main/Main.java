d80a97760e7281f44da4735719a65790a8810cd4
reedfenters <rfenters95@gmail.com>
2017-08-26 08:13:46 -0400

    minor update

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/main/ui/root/Root.fxml"));
-        primaryStage.setTitle("RRTT - Alpha");
+        primaryStage.setTitle("RRTT - EARLY BUILD");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
