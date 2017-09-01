f75459d125f9a992c336d6a0e1032d9a18e5304c
reedfenters <rfenters95@gmail.com>
2017-09-01 10:51:02 -0400

    added documentation

--- src/main/core/drive/listeners/AbstractDriveListener.java
+++ src/main/core/drive/listeners/AbstractDriveListener.java
@@ -4,12 +4,29 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.beans.value.ChangeListener;
 import main.core.drive.modes.AbstractDriveMode;
 
+/*
+* Drive listeners in general are custom ChangeListeners for the DriveTabController.
+* They check both the value and format for velocity and radius for drive/driveDirect
+* command parameters.
+* */
 public abstract class AbstractDriveListener implements ChangeListener<String> {
 
+  /* *********************************************
+  *
+  * Package-private members
+  *
+  ********************************************** */
   AbstractDriveMode driveMode;
   AbstractDriveMode.Position position;
   JFXTextField textField;
 
+  /*
+  * Constructs AbstractDriveListener
+  *
+  * @param driveMode determines which Roomba Drive command to configure for.
+  * @param position determines which parameter given the Drive command to configure for.
+  * @param textField reference of TextField of which listener is attached.
+  * */
   AbstractDriveListener(AbstractDriveMode driveMode, AbstractDriveMode.Position position,
       JFXTextField textField) {
     this.driveMode = driveMode;
@@ -18,18 +35,39 @@ public abstract class AbstractDriveListener implements ChangeListener<String> {
     this.textField.textProperty().addListener(this);
   }
 
+  /*
+  * Check input length
+  *
+  * @param value Extracted TextField input.
+  * @return True if value's length is 5 or less.
+  * */
   boolean hasInValidLength(String value) {
     return !value.matches("\\d{0,5}?");
   }
 
+  /*
+  * @param value Extracted TextField input.
+  * @return value without leading zeros
+  * */
   private String removeLeadingZeros(String value) {
     return value.replaceFirst("^0+", "");
   }
 
+  /*
+  * @param value Extracted TextField input.
+  * @return True if value has leading zeros
+  * */
   private boolean hasLeadingZeros(String value) {
     return value.startsWith("0") && value.length() > 1;
   }
 
+  /*
+  * Removes leading zeros, if any, and sets
+  * empty String to zero (valid input).
+  *
+  * @param value Extracted TextField input.
+  * @return True if value should be passed to value validation
+  * */
   boolean format(String value) {
     if (value.isEmpty()) {
       textField.setText("0");
