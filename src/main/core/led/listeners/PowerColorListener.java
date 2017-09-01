14259fe6d34117f1c610e63a1d75e8e170ece013
reedfenters <rfenters95@gmail.com>
2017-09-01 11:33:53 -0400

    added documentation

--- src/main/core/led/listeners/PowerColorListener.java
+++ src/main/core/led/listeners/PowerColorListener.java
@@ -2,8 +2,19 @@ package main.core.led.listeners;
 
 import com.jfoenix.controls.JFXTextField;
 
+/*
+* Custom ChangeListener for the LightTabController.
+* Checks both the value and format for PowerColor values in Roomba LED
+* command parameters.
+* */
 public class PowerColorListener extends AbstractLEDListener {
 
+  /*
+  * Constructs PowerColorListener
+  *
+  * @param promptText promptText for TextField.
+  * @param textField reference of TextField of which listener is attached.
+  * */
   public PowerColorListener(JFXTextField textField) {
     super("Power Color", textField);
   }
--- src/main/core/led/listeners/PowerIntensityListener.java
+++ src/main/core/led/listeners/PowerIntensityListener.java
@@ -2,9 +2,19 @@ package main.core.led.listeners;
 
 import com.jfoenix.controls.JFXTextField;
 
-
+/*
+* Custom ChangeListener for the LightTabController.
+* Checks both the value and format for PowerIntensity values in Roomba LED
+* command parameters.
+* */
 public class PowerIntensityListener extends AbstractLEDListener {
 
+  /*
+  * Constructs PowerIntensityListener
+  *
+  * @param promptText promptText for TextField.
+  * @param textField reference of TextField of which listener is attached.
+  * */
   public PowerIntensityListener(JFXTextField textField) {
     super("Power Intensity", textField);
   }
