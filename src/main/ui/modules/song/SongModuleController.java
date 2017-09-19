ad9f2963a49e7d6c7fd43dd48736e3908e91e1fc
reedfenters <rfenters95@gmail.com>
2017-09-19 16:55:25 -0400

    created a NoteControl (must import into Scene Builder), all controls can be modified concurrently.

--- src/main/ui/modules/song/SongModuleController.java
+++ src/main/ui/modules/song/SongModuleController.java
@@ -2,6 +2,7 @@ package main.ui.modules.song;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ScrollPane;
@@ -15,6 +16,15 @@ public class SongModuleController implements Initializable, Injectable {
   private ScrollPane songModule;
 
   private RootController rootController;
+
+  /*
+  * Consider NoteParameter class / custom control
+  * then adding NoteParameters to GridPane programmatically.
+  * */
+  @FXML
+  public void playNote(ActionEvent event) {
+    System.out.println("Yolo!");
+  }
 
   @Override
   public void inject(RootController rootController) {
---
+++ src/main/ui/modules/song/note/NoteControl.java
@@ -0,0 +1,69 @@
+package main.ui.modules.song.note;
+
+import com.jfoenix.controls.JFXComboBox;
+import java.io.IOException;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class NoteControl extends HBox {
+
+  @FXML
+  private JFXComboBox<String> noteComboBox;
+
+  @FXML
+  private JFXComboBox<String> durationComboBox;
+
+  @FXML
+  private Label noteLabel;
+
+  public NoteControl() {
+
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+        "Note.fxml"));
+    fxmlLoader.setRoot(this);
+    fxmlLoader.setController(this);
+
+    try {
+      fxmlLoader.load();
+    } catch (IOException exception) {
+      throw new RuntimeException(exception);
+    }
+
+    populateNoteComboBox();
+    populateDurationComboBox();
+
+  }
+
+  public String getText() {
+    return textProperty().get();
+  }
+
+  public void setText(String value) {
+    textProperty().set(value);
+  }
+
+  public StringProperty textProperty() {
+    return noteLabel.textProperty();
+  }
+
+  private void populateNoteComboBox() {
+    noteComboBox.getItems().add("A");
+    noteComboBox.getItems().add("B");
+    noteComboBox.getItems().add("C");
+    noteComboBox.getItems().add("D");
+    noteComboBox.getItems().add("E");
+    noteComboBox.getItems().add("F");
+    noteComboBox.getItems().add("G");
+  }
+
+  private void populateDurationComboBox() {
+    for (int i = 0; i < 64; i++) {
+      durationComboBox.getItems().add(String.valueOf(i) + "/ 64 second");
+    }
+    durationComboBox.getItems().add("1 second");
+  }
+
+}
